97b2787c1850151750dc28b66db95dd52aeb1ca4
alexgn023@gmail.com <alexgn023@gmail.com>
2025-06-26 17:43:25 +0200

    js pokemon

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,19 +1,22 @@
 import { baseURL } from "./config.js";
 
-export async function callAPI(path, option ={}) {
+export async function callAPI(path, options ={}) {
     const url = `${baseURL}${path}`;
+
 
     try{
         const res = await fetch(url,{
+            
+           
             headers: {"Content-Type": "application/json"},
-            ...option
+            ...options
         });
         if(!res.ok) throw new Error(`Error ${res.status}`);
+
         return await res.json();
     }catch (err){
-        console.error("APi Error: ", err);
+        console.error("API Error: ",err);
         throw err;
     }
 
-    
-};
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 import { callAPI } from "./api.js";
 
+const pokemon = document.getElementById('pokemon');
+const btnPok = document.getElementById('btnPok');
+const pokemonCard = document.getElementById('pokemon-card');
+const output = document.getElementById('output');
+
+btnPok.addEventListener('click', ()=>{
+   
+    callAPI(`/pokemon/${name}`)
+    .then(data=>{
+        output.textContent= JSON.stringify(data,null,2);
+    })
+    .catch(err =>{
+        output.textContent = "Error: " + err.message;
+    });
+
+
+});
